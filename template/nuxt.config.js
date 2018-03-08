b0553f7e4fcd6417c7bd40a31bcd4d3ee7e52fca
Dan Lynch <pyramation@gmail.com>
2018-03-08 13:05:52 -0800

    added latex2vue

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -7,12 +7,19 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: '{{escape description }}',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+
+  css: ['latex2js/lib/latex2js.css'],
+
+  plugins: [{ src: '~plugins/latex2vue.js', ssr: false }],
+
   /*
   ** Customize the progress bar color
   */
@@ -24,15 +31,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
--- template/pages/index.vue
+++ template/pages/index.vue
@@ -8,7 +8,12 @@
       <h2 class="subtitle">
         {{ description }}
       </h2>
+      <latex :content="tex" />
       <div class="links">
+        <a
+          href="https://github.com/pyramation/LaTeX2JS"
+          target="_blank"
+          class="button--grey">LaTeX2Vue</a>
         <a
           href="https://nuxtjs.org/"
           target="_blank"
@@ -23,13 +28,80 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
+import AppLogo from '~/components/AppLogo.vue';
 export default {
   components: {
-    AppLogo
-  }
-}
+    AppLogo,
+  },
+  data() {
+    return {
+      tex: String.raw`
+      Probably the best part of using PSTricks is that you can mix both graphics and mathematics:
+
+      \begin{center}
+      \begin{pspicture}(0,-3)(8,3)
+
+      \rput(0,0){$x(t)$}
+      \rput(4,1.5){$f(t)$}
+      \rput(4,-1.5){$g(t)$}
+      \rput(8.2,0){$y(t)$}
+      \rput(1.5,-2){$h(t)$}
+
+      \psframe(1,-2.5)(7,2.5)
+      \psframe(3,1)(5,2)
+      \psframe(3,-1)(5,-2)
+
+      \rput(4,0){$X_k = \frac{1}{p} \sum \limits_{n=\langle p\rangle}x(n)e^{-ik\omega_0n}$}
+
+      \psline[linewidth=1.25 pt]{->}(0.5,0)(1.5,0)
+      \psline[linewidth=1.25 pt]{->}(1.5,1.5)(3,1.5)
+      \psline[linewidth=1.25 pt]{->}(1.5,-1.5)(3,-1.5)
+
+      \psline[linewidth=1.25 pt]{->}(6.5,1.5)(6.5,0.25)
+      \psline[linewidth=1.25 pt]{->}(6.5,-1.5)(6.5,-0.25)
+      \psline[linewidth=1.25 pt]{->}(6.75,0)(7.75,0)
+      \psline[linewidth=1.25 pt](1.5,-1.5)(1.5,1.5)
+      \psline[linewidth=1.25 pt](5,1.5)(6.5,1.5)
+      \psline[linewidth=1.25 pt](5,-1.5)(6.5,-1.5)
+      \psline[linewidth=1.25 pt](6,-1.5)(6.5,-1.5)
+
+      \pscircle(6.5,0){0.25}
+      \psline(6.25,0)(6.75,0)
+      \psline(6.5,0.5)(6.5,-0.5)
+
+      \end{pspicture}
+      \end{center}
+
+      which can be produced with the following code:
+
+      \begin{verbatim}
+      \rput(0,0){$x(t)$}
+      \rput(4,1.5){$f(t)$}
+      \rput(4,-1.5){$g(t)$}
+      \rput(8.2,0){$y(t)$}
+      \rput(1.5,-2){$h(t)$}
+      \psframe(1,-2.5)(7,2.5)
+      \psframe(3,1)(5,2)
+      \psframe(3,-1)(5,-2)
+      \rput(4,0){$X_k = \frac{1}{p} \sum \limits_{n=\langle p\rangle}x(n)e^{-ik\omega_0n}$}
+      \psline[linewidth=1.25 pt]{->}(0.5,0)(1.5,0)
+      \psline[linewidth=1.25 pt]{->}(1.5,1.5)(3,1.5)
+      \psline[linewidth=1.25 pt]{->}(1.5,-1.5)(3,-1.5)
+      \psline[linewidth=1.25 pt]{->}(6.5,1.5)(6.5,0.25)
+      \psline[linewidth=1.25 pt]{->}(6.5,-1.5)(6.5,-0.25)
+      \psline[linewidth=1.25 pt]{->}(6.75,0)(7.75,0)
+      \psline[linewidth=1.25 pt](1.5,-1.5)(1.5,1.5)
+      \psline[linewidth=1.25 pt](5,1.5)(6.5,1.5)
+      \psline[linewidth=1.25 pt](5,-1.5)(6.5,-1.5)
+      \psline[linewidth=1.25 pt](6,-1.5)(6.5,-1.5)
+      \pscircle(6.5,0){0.25}
+      \psline(6.25,0)(6.75,0)
+      \psline(6.5,0.5)(6.5,-0.5)
+      \end{verbatim}
+      `,
+    };
+  },
+};
 </script>
 
 <style>
@@ -42,7 +114,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
