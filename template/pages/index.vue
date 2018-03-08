b9c217cab99bf3eb172112068cdc65789b0bed2d
Dan Lynch <pyramation@gmail.com>
2018-03-08 13:14:09 -0800

    keep it simple

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,38 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        {{ name }}
-      </h1>
-      <h2 class="subtitle">
-        {{ description }}
-      </h2>
-      <latex :content="tex" />
-      <div class="links">
-        <a
-          href="https://github.com/pyramation/LaTeX2JS"
-          target="_blank"
-          class="button--grey">LaTeX2Vue</a>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <latex :content="tex" />
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue';
 export default {
-  components: {
-    AppLogo,
-  },
   data() {
     return {
       tex: String.raw`
@@ -105,33 +76,5 @@ export default {
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
